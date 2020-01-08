c53323f6de6759985d53678248f7279847a55cf1
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-01-07 20:11:28 -0800

    Fix for request headers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,7 +19,7 @@ return jwt.sign(payload,'aeaeiouAndSometimesY', options);
 
 router.post('/register', (req, res) => {
  
-  const { username, password } = req.headers;
+  const { username, password } = req.body;
   Users.insert({ username, password: bcrypt.hashSync(password, 8) })
     .then(id => {
       res.status(201).json({ message: "User registration complete", id });
@@ -32,7 +32,7 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
  
-  const { username, password } = req.headers;
+  const { username, password } = req.body;
   Users
     .findByUsername(username)
     .then(user => {
